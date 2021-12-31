4ba3742340fdd235dfefa5fd36532e99aa9a4cc6
Sushant Ranjan <er.sushantranjan@gmail.com>
2021-12-31 13:54:28 +0530

    mkaing it rain

--- supportFiles/script/script.js
+++ supportFiles/script/script.js
@@ -1,10 +1,39 @@
+const containerEl = document.querySelector(".container")
+// const pEl = document.querySelector(".canvas-details")
 const textEl = document.querySelector(".auto-text")
+const redPill = document.querySelector(".red-pill-btn")
 
 
+// pEl.style.display = "none"
 const text = "Welcome to the Matrix"
 let idx = 1
 
-// writeText()
+const canvas = document.querySelector('#matrix')
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const context = canvas.getContext('2d');
+const fontSize = 16;
+const columns = canvas.width/fontSize;
+
+canvas.id = "matrix"
+canvas.style.display = "none"
+// console.log(canvas.width)
+
+
+const katakana = 'アァカサタナハマヤャラワガザダバパイィキシチニヒミリヰギジヂビピウゥクスツヌフムユュルグズブヅプエェケセテネヘメレヱゲゼデベペオォコソトノホモヨョロヲゴゾドボポヴッン';
+const latin = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const nums = '0123456789';
+const alphabet = katakana + latin + nums;
+
+
+const rainDrops = [];
+
+// Typewriter effect
+
+writeText()
+
+// pEl.innerText = canvas.width + "," + canvas.height
 
 function writeText() {
     textEl.innerText = text.slice(0, idx)
@@ -15,7 +44,48 @@ function writeText() {
         idx = 1
     }
 
-    // setTimeout(writeText, 300)
+    setTimeout(writeText, 150)
 }
 
-setInterval(writeText, 150)
+// setInterval(writeText, 150)
+
+//MAKING IT RAIN
+
+function makeItRain() {
+    for( let x = 0; x < columns; x++ ) {
+        rainDrops[x] = 1;
+    }
+    
+    const draw = () => {
+        context.fillStyle = 'rgba(0, 0, 0, 0.05)';
+        context.fillRect(0, 0, canvas.width, canvas.height);
+        
+        context.fillStyle = '#0F0';
+        context.font = fontSize + 'px monospace';
+    
+        for(let i = 0; i < rainDrops.length; i++)
+        {
+            const text = alphabet.charAt(Math.floor(Math.random() * alphabet.length));
+            context.fillText(text, i*fontSize, rainDrops[i]*fontSize);
+            
+            if(rainDrops[i]*fontSize > canvas.height && Math.random() > 0.975){
+                rainDrops[i] = 0;
+            }
+            rainDrops[i]++;
+        }
+    };
+    
+    setInterval(draw, 30);
+}
+
+// Red Pill functionality
+
+function loadRedPillPage() {
+    // pEl.innerText = canvas.width + "," + canvas.height
+    containerEl.style.display = "none"
+    canvas.style.display = "block"
+    // containerEl.remove()
+    makeItRain()
+}
+
+redPill.addEventListener("click", loadRedPillPage)
